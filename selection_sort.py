9e677bf0e895f0d62d4b888da4e6282f04b52967
Adish K T <161046695+adishkt@users.noreply.github.com>
2026-06-26 12:18:32 +0530

    update

--- selection_sort.py
+++ selection_sort.py
@@ -11,3 +11,17 @@ def selection_sort(nums):
 
 nums=[5,7,8,4,1,6,9,2]
 print(selection_sort(nums))
+
+
+def sort(arr):
+    q=len(arr)
+    for i in range(0,q):
+        max=i
+        for j in range(i+1,q):
+            if arr[j]>arr[max]:
+                max=j
+        arr[i],arr[max]=arr[max],arr[i]
+    return arr
+
+arr=[5,7,8,4,1,6,9,2]
+print(sort(arr))
