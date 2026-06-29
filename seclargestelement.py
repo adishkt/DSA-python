af1ef938fd1e96b8760ed64199d01a121ce1d11d
Adish K T <161046695+adishkt@users.noreply.github.com>
2026-06-29 22:15:35 +0530

    better solution for for second largest element in array

--- seclargestelement.py
+++ seclargestelement.py
@@ -2,5 +2,15 @@ nums=[55,32,97,-55,45,32,88,21]
 #Brute force way
 nums1=nums
 nums1.sort()
-n=len(nums1)
+n=len(nums)
 print(nums1[n-2])
+
+#better solution
+largest=float("-inf")
+s_largest=float("-inf")
+for i in range(0,n):
+    largest=max(largest,nums[i])
+for i in range(0,n):
+    if nums[i]>s_largest and nums[i]!=largest:
+        s_largest=nums[i]
+print(s_largest)
