83ec465243931729a27d29244bb8eaf48b6ebe52
Adish K T <161046695+adishkt@users.noreply.github.com>
2026-08-17 20:40:49 +0530

    upperbound done

---
+++ upperBound.py
@@ -0,0 +1,15 @@
+nums=[1,1,1,2,3,3,5,6,7,7,7,9,12,12,13]
+target=int(input("ENter Number"))
+n=len(nums)
+ub=n
+low=0
+high=n-1
+while low<=high:
+    mid=(low+high)//2
+    if nums[mid]>target:
+        ub=mid
+        high=mid-1
+    else:
+        low=mid+1
+print(ub)
+        
